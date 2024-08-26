e3781d8d702ed5c90ad9dc25565dbcb162dd9da6
Bushra Patel <bpatel60@yahoo.com>
2024-08-26 18:54:11 -0400

    Implement different methods in the filters.rb file according to compass exercise

--- filters.rb
+++ filters.rb
@@ -3,14 +3,45 @@
 
 def find(id)
     # Your code Here
+    @candidates.each do |candidate|
+      if candidate[:id] == id 
+        return candidate
+      end
+    end
+    nil
   end
   
   def experienced?(candidate)
     # Your code Here
+    if candidate[:years_of_experience] >= 2
+      true
+    else
+      false
+    end
   end
   
   def qualified_candidates(candidates)
     # Your code Here
+    candidates.filter do |candidate|
+      if experienced?(candidate) && candidate[:github_points] >= 100
+        if ruby_or_python(candidate)
+          applied?(candidate) && candidate[:age] > 17
+            candidate
+        end
+      end
+    end
   end
   
   # More methods will go below
+  
+  def applied?(candidate)
+    candidate[:date_applied] < 15.days.ago.to_date
+  end
+  
+  def ruby_or_python(candidate)
+    candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+  end
+
+  def order_by_qualifications(candidates)
+    sorted_candidates = candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+  end
--- main.rb
+++ main.rb
@@ -7,3 +7,15 @@ require './filters'
 ## Your test code can go here
 
 pp @candidates
+
+p "Is Experienced?"
+pp experienced?(@candidates[0])
+
+p "Find by id:"
+pp find(9)
+
+p "Qualified Candidates:"
+pp qualified_candidates(@candidates)
+
+p "Order By Qualifications:"
+pp order_by_qualifications(@candidates)
